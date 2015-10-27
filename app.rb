73fcbd07176c7ffe67f071344b37c88211a414cf
Fergus Lemon <fergus.lemon@gmail.com>
2015-10-27 17:50:23 +0000

    feature tests fo capybara - boom

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require 'sinatra/base'
+require 'shotgun'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    erb(:index)
   end
 
+  post '/names' do
+    @Player1 = params[:Player1]
+    @Player2 = params[:Player2]
+    erb(:start)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features/feature_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'user management' do
+
+  scenario 'start page has player name input fields' do
+    visit '/'
+    expect(page).to have_text("Player1 Player2")
+  end
+
+  scenario 'submit button takes you to the next page' do
+    visit '/'
+    click_button 'submit'
+    expect(current_path).to eq("/names")
+  end
+
+  scenario 'players input names, takes you to the next page and includes content' do
+    visit '/'
+    fill_in 'Player1', :with => 'Chuka'
+    fill_in 'Player2', :with => 'Fergus'
+    click_button 'submit'
+    expect(page).to have_content("Chuka vs Fergus")
+  end
+
+end
